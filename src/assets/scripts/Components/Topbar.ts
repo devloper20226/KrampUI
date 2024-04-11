ec21e462be218a68b9dccb3d95ffce26007a4a66
Pixeluted <pixeluted@proton.me>
2024-04-11 15:53:03 +0200

    Added help button in topbar

--- src/assets/scripts/Components/Topbar.ts
+++ src/assets/scripts/Components/Topbar.ts
@@ -1,11 +1,16 @@
 import { appWindow } from "@tauri-apps/api/window";
 import { exit } from "../main";
+import { open } from "@tauri-apps/api/shell";
 
 export default async function () {
     const minimizeButton = document.querySelector(".tb-button.minimize") as Element;
     const maximazeButton = document.querySelector(".tb-button.maximize") as Element;
     const exitButton = document.querySelector(".tb-button.exit") as Element;
+    const helpButton = document.querySelector(".help") as Element;
     minimizeButton.addEventListener("click", appWindow.minimize);
     maximazeButton.addEventListener("click", appWindow.maximize);
     exitButton.addEventListener("click", exit);
+    helpButton.addEventListener("click", () => {
+        open("https://www.youtube.com/watch?v=dQw4w9WgXcQ")
+    })
 }
